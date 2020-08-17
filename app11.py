cc2dff4806dd8d65fa8c65eec13f7d2294490991
luarn <luarn@gapps.ntust.edu.tw>
2020-08-17 17:53:00 +0800

    “更新2”

--- app11.py
+++ app11.py
@@ -41,7 +41,7 @@ def callback():
 
     return 'OK'
 
-# 學你說話
+# 處理輸入的文字
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     
@@ -59,6 +59,184 @@ def handle_message(event):
         preview_image_url='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/preview.jpg'))
     elif event.message.text == u"音訊":
         line_bot_api.reply_message(event.reply_token,AudioSendMessage(original_content_url='https://s3-ap-northeast-1.amazonaws.com/dazedbear-assets/Bluebird.mp3', duration=100000))
+    elif event.message.text == "位置":
+        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='我的位置', address='台科大', latitude=25.013277, longitude=121.540617))
+    elif event.message.text == "位置2":
+        imagemap_message = ImagemapSendMessage(
+                        base_url='https://www.kamigo.tw/assets/kamigo-c3b10dff4cdb60fa447496b22edad6c32fffde96de20262efba690892e4461e8.png#',
+                        alt_text='this is an imagemap',
+                        base_size=BaseSize(height=1040, width=1040),
+                        actions=[
+                            URIImagemapAction(
+                                link_uri='https://www.kamigo.tw/',
+                                area=ImagemapArea(
+                                    x=0, y=0, width=520, height=1040
+                                )
+                            ),
+                            MessageImagemapAction(
+                                text='hello',
+                                area=ImagemapArea(
+                                    x=520, y=0, width=520, height=1040
+                                )
+                            )
+                        ]
+                    )
+        line_bot_api.reply_message(event.reply_token,imagemap_message)
+    elif event.message.text == "樣板":    
+        buttons_template = TemplateSendMessage(
+        alt_text='目錄 template',
+        template=ButtonsTemplate(
+            title='Template-樣板介紹',
+            text='Template分為四種，也就是以下四種：',
+            thumbnail_image_url='圖片網址',
+            actions=[
+                MessageTemplateAction(
+                    label='Buttons Template',
+                    text='Buttons Template'
+                ),
+                MessageTemplateAction(
+                    label='Confirm template',
+                    text='Confirm template'
+                ),
+                MessageTemplateAction(
+                    label='Carousel template',
+                    text='Carousel template'
+                ),
+                MessageTemplateAction(
+                    label='Image Carousel',
+                    text='Image Carousel'
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template)
+    elif event.message.text == "Buttons Template":       
+        buttons_template = TemplateSendMessage(
+        alt_text='Buttons Template',
+        template=ButtonsTemplate(
+            title='這是ButtonsTemplate',
+            text='ButtonsTemplate可以傳送text,uri',
+            thumbnail_image_url='https://myalbum.com/photo/zmsT9qdUgesL/360.jpg',
+            actions=[
+                MessageTemplateAction(
+                    label='ButtonsTemplate',
+                    text='ButtonsTemplate'
+                ),
+                URITemplateAction(
+                    label='VIDEO1',
+                    uri='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/video.mp4'
+                ),
+                PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='postback1'
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template)
+    elif event.message.text == "Carousel template":
+        print("Carousel template")       
+        Carousel_template = TemplateSendMessage(
+        alt_text='目錄 template',
+        template=CarouselTemplate(
+        columns=[
+            CarouselColumn(
+                thumbnail_image_url='https://myalbum.com/photo/V2T4Ev5oRsku/360.jpg',
+                title='this is menu1',
+                text='description1',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback1',
+                        text='postback text1',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='message1',
+                        text='message text1'
+                    ),
+                    URITemplateAction(
+                        label='station',
+                        uri='https://www.railway.gov.tw/tra-tip-web/tip'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                thumbnail_image_url='https://myalbum.com/photo/H8YwbUE48wEm/360.jpg',
+                title='this is menu2',
+                text='description2',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback2',
+                        text='postback text2',
+                        data='action=buy&itemid=2'
+                    ),
+                    MessageTemplateAction(
+                        label='message2',
+                        text='message text2'
+                    ),
+                    URITemplateAction(
+                        label='food panda',
+                        uri='https://www.foodpanda.com.tw/'
+                    )
+                ]
+            )
+        ]
+    )
+    )
+        line_bot_api.reply_message(event.reply_token,Carousel_template)
+    elif event.message.text == "Confirm template":
+        print("Confirm template")       
+        Confirm_template = TemplateSendMessage(
+        alt_text='目錄 template',
+        template=ConfirmTemplate(
+            title='這是ConfirmTemplate',
+            text='這就是ConfirmTemplate,用於兩種按鈕選擇',
+            actions=[                              
+                PostbackTemplateAction(
+                    label='Y',
+                    text='Y',
+                    data='action=buy&itemid=1'
+                ),
+                MessageTemplateAction(
+                    label='N',
+                    text='N'
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token,Confirm_template)
+    elif event.message.text == "Image Carousel":
+        print("Image Carousel")       
+        Image_Carousel = TemplateSendMessage(
+        alt_text='Image Carousel template',
+        template=ImageCarouselTemplate(
+        columns=[
+            ImageCarouselColumn(
+                image_url='https://myalbum.com/photo/u9SnsjctSiCs/360.jpg',
+                action=PostbackTemplateAction(
+                    label='postback1',
+                    text='postback text1',
+                    data='action=buy&itemid=1'
+                )
+            ),
+            ImageCarouselColumn(
+                image_url='https://myalbum.com/photo/RQcPjmBSrUNR/360.jpg',
+                action=PostbackTemplateAction(
+                    label='postback2',
+                    text='postback text2',
+                    data='action=buy&itemid=2'
+                )
+            )
+        ]
+    )
+    )
+        line_bot_api.reply_message(event.reply_token,Image_Carousel)
+
+
+
+
+
     else:
         line_bot_api.reply_message(event.reply_token,message)
 
